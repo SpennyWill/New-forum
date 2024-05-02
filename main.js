429911e6d04031f307831b9d0a9154a03b12dd49
Spencer Willis <spencer.willis220@gmail.comgit config --global user.email spencer.willis220@gmail.comgit statusgit status>
2024-05-01 20:48:01 -0400

    stuck again

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ document.getElementById('.submit').addEventListener('click', (event) => {
   var postName = document.getElementById('name').value;
   var postMessage = document.getElementById('message').value;
 
-  let nameHeading = document.createElement('h2'); 
-  let messageHeading = document.createElement('h3'); 
+  let nameSetter = document.createElement('h2'); 
+  let messageSetter = document.createElement('h3'); 
 
   document.getElementsByClassName('posts').appendChild(nameHeading);
 
