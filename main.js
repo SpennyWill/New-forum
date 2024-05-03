8fc67bfe55bb68bfd8c9b901e3f69a94fd673934
Spencer Willis <spencer.willis220@gmail.comgit config --global user.email spencer.willis220@gmail.comgit statusgit status>
2024-05-02 20:00:03 -0400

    finished posting function

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-document.getElementById('.submit').addEventListener('click', (event) => {
+document.getElementById('submit').addEventListener('click', (event) => {
   event.preventDefault(); 
   
   var postName = document.getElementById('name').value;
   var postMessage = document.getElementById('message').value;
 
-  let nameSetter = document.createElement('h2'); 
-  let messageSetter = document.createElement('h3'); 
+  let nameHeader = document.createElement('h2') 
+  let messageBody = document.createElement('p'); 
 
-  document.getElementsByClassName('posts').appendChild(nameHeading);
+   document.getElementsByClassName('posts')[0].appendChild(nameHeader);
+nameHeader.innerHTML = postName; 
 
-  document.getElementsByClassName('posts').appendChild(nameHeading);
+  document.getElementsByClassName('posts')[0].appendChild(messageBody)
+  messageBody.innerHTML = postMessage; 
+  
+  document.getElementById('message').value = '';
 
+  document.getElementById('name').value = '';
 
 })
 
